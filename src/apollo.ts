b843035bb0bae32324f6a6cfe1979c65e44c2b94
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-09 23:34:45 +0300

    Logo

--- src/apollo.ts
+++ src/apollo.ts
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 
 let uri = "";
 if (process.env.NODE_ENV === "development") {
-  uri = "https://deskpics.now.sh/graphql";
+  uri = "http://localhost:4000/graphql";
 } else {
   uri = "https://deskpics.now.sh/graphql";
 }
